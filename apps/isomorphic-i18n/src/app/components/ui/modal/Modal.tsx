b1cc8ce3ed861b2defe5663f0b5805faae9d5549
MohamedMamdouh2002 <mm4871859@gmail.com>
2025-03-30 08:08:48 +0200

    feat:merge24

--- apps/isomorphic-i18n/src/app/components/ui/modal/Modal.tsx
+++ apps/isomorphic-i18n/src/app/components/ui/modal/Modal.tsx
@@ -216,6 +216,38 @@ function Modal({
     fetchData();
   }, [GetProduct, modalId]);
 
+  useEffect(() => {
+    let scrollY = 0;
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  
+    if (modalId) {
+      scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.left = '0';
+      document.body.style.right = '0';
+      document.body.style.overflow = 'hidden';
+      document.body.dataset.scrollY = scrollY.toString();
+    }
+  
+    return () => {
+      if (isIOS) {
+        const storedY = parseInt(document.body.dataset.scrollY || '0');
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.left = '';
+        document.body.style.right = '';
+        document.body.style.overflow = '';
+        window.scrollTo(0, storedY);
+      } else {
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.left = '';
+        document.body.style.right = '';
+        document.body.style.overflow = '';
+      }
+    };
+  }, [modalId]);
   const handleAddToCart = () => {
     if (!prodCartItem) return;
     const cartItem: CartItem = {
