0693d50d1d49e7f67b7aabba9ccaec8fd70ba634
MohamedMamdouh2002 <mm4871859@gmail.com>
2025-04-24 20:03:49 +0200

    feat:convert to server2

--- apps/isomorphic-i18n/src/app/[lang]/(hydrogen)/reviews/page.tsx
+++ apps/isomorphic-i18n/src/app/[lang]/(hydrogen)/reviews/page.tsx
@@ -27,7 +27,7 @@ async function Review({ params: { lang } }: { params: { lang: string } }) {
 
   return (
     <div className="w-[90%] mx-auto">
-      <Reviews lang={lang} reviews={reviews as any} />
+      {/* <Reviews lang={lang} reviews={reviews as any} /> */}
     </div>
   );
 }
